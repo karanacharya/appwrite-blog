b1f1605b6526eb0b6793739d919b09eb70cec4cf
karanacharya <karanacharya0987@gmail.com>
2024-08-30 18:03:30 +0530

    first commit
    Done making the env variables .The next step is to use the authentication service of appwrite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  
+  return (
+    <>
+      <h1> Welcome to  Appwrite blog</h1>
+    </>
+  )
+}
+
+export default App
