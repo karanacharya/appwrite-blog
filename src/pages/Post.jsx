cf6d509f39a44d7ceb481d9b65ed9412a2dc0b0f
karanacharya <karanacharya0987@gmail.com>
2024-09-23 20:38:56 +0530

    Could not resolve "../appwrite/config" from "src/pages/Post.jsx"

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import appwriteService from "../appwrite/config";
+import appwriteService from "../Appwrite/config";
 import { Button, Container } from "../components/index";
 import parse from "html-react-parser";
 import { useSelector } from "react-redux";
